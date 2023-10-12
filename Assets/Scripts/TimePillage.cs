77d1a3a9c19ad94fa66e149c420a9bda1f9dead0
walleraa <walleraa@umich.edu>
2023-10-12 18:44:48 -0400

    Refactored how pillaging works to fix bug with multiple ships

--- Assets/Scripts/TimePillage.cs
+++ Assets/Scripts/TimePillage.cs
@@ -7,40 +7,51 @@ public class TimePillage : MonoBehaviour
     public float pillage_counter = 5f;
 
     private float time_start = 0f;
-    private bool being_pillaged = false;
+    private float frames_per_second = 0f;
+    private int pirates_pillaging = 0;
 
-    private void Update()
+    private bool exists = true;
+
+    private void Start()
     {
-        // Disable the gold once it's been fully pillaged
-        if (Time.time - time_start > pillage_counter && being_pillaged)
+        frames_per_second = 1 / Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        // The pirates have spent enough time pillaging
+        if (time_start >= pillage_counter)
         {
+            // For debug purposes only
+            if (exists)
+            {
+                Debug.Log("End Pillaging");
+                exists = false;
+            }
+
+            // Disable the renderer and collider
             GetComponent<MeshRenderer>().enabled = false;
             GetComponent<MeshCollider>().enabled = false;
-        }
-        // Reset knowledge on a pirate being there so if they disappear the pillaging stops
-        else if (being_pillaged)
-        {
-            being_pillaged = false;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Prevent the counter from restarting when another pirate arrives
-        if (!being_pillaged && collision.gameObject.layer == 7)
-        {
-            being_pillaged = true;
-            time_start = Time.time;
-        }
-        else if (collision.gameObject.layer == 7)
-        {
-            being_pillaged = false;
-        }
+        Debug.Log("Start pillaging");
+        pirates_pillaging += 1;
+        time_start += 1 / (frames_per_second * pirates_pillaging);
     }
 
     // Keep checking if there's a pirate there
     private void OnCollisionStay(Collision collision)
     {
-        being_pillaged = true;
+        //being_pillaged = true;
+        time_start += 1 / (frames_per_second * pirates_pillaging);
+    }
+
+    // Sometimes the pirates slide off the collider
+    private void OnCollisionExit(Collision collision)
+    {
+        pirates_pillaging -= 1;
     }
 }
